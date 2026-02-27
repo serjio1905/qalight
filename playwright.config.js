b1cab7935d39a253b76a3dd9040aeb1dfd8fc7e9
olgarubanova <olrub23@gmail.com>
2026-02-27 13:35:46 +0200

    tmp

--- playwright.config.js
+++ playwright.config.js
@@ -22,7 +22,11 @@ module.exports = defineConfig({
     /* Opt out of parallel tests on CI. */
     workers: process.env.CI ? 1 : undefined,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-    reporter: [["html"], ["list"], ["json", { outputFile: "test-results/results.json" }]],
+    reporter: [
+        ["html", { open: "never", stepDetails: false }],
+        ["list"],
+        ["json", { outputFile: "test-results/results.json" }],
+    ],
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
         /* Base URL to use in actions like `await page.goto('/')`. */
