7666a0d2e3708736c7a77dc74f92eead37346ca6
olgarubanova <olrub23@gmail.com>
2026-03-04 21:28:25 +0200

    api

--- src/qa.js
+++ src/qa.js
@@ -491,7 +491,7 @@ export class QA {
                 identifiersString += `, ${key}*="${value}"`;
             }
             identifiersString += `]`;
-            const targetElement = this.currentElement.locator.locator(tag, { hasText: text });
+            const targetElement = this.page.locator(tag, { hasText: text });
             await this._scrollContairnerUntilTargetVisible(this.currentElement.locator, targetElement, { direction });
         } catch (error) {
             await this._showHint(`Error scrolling to ${text}`, "error");
