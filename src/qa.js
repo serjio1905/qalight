a753e827cf0da28f63539f5dc6d8e21f6f099ea2
olgarubanova <olrub23@gmail.com>
2026-03-21 16:43:41 +0200

    tmp

--- src/qa.js
+++ src/qa.js
@@ -177,6 +177,10 @@ export class QA {
     }
 
     async abort(msg = this._abortMessage) {
+        if (this._pauseResolver) {
+            this._pauseResolver();
+            this._pauseResolver = null;
+        }
         await this._showHint(msg || "Aborted by user.", "error");
         await this.waitFor(3000, false);
         throw new QAError(`Failed to execute action.${this.safeMode ? " Aborted by user." : ""}`);
