7721869ee02fa2d6d394fe64079814f9e736c35f
olgarubanova <olrub23@gmail.com>
2026-03-22 17:57:03 +0200

    tmp

--- src/points.js
+++ src/points.js
@@ -14,6 +14,7 @@ export class WeightPointCalculator {
         columnName: 10,
         columnThHtml: 10,
         placeholder: 8,
+        title: 8,
         label: 8,
         html: 6,
         class: 5,
